be91f49480118482cfa60861fe55b30430b3828d
Kayla72123 <130508537+Kayla72123@users.noreply.github.com>
2024-10-21 15:01:03 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+let position;
+let velocity;
+let acceleration;
+
+
+function setup() {
+ // put setup code here
+  createCanvas(800, 800);
+  background(255);
+
+  position = createVector(100,100); // other way of creating vectors 
+  velocity = createVector(2.5, .5);
+  stroke(255);
+  strokeWeight(2);
+  fill(127,127);
+
+
+}
+
+function draw() {
+  background(0);
+
+  position.add(velocity);
+
+  ellipse(position.x, position.y, 20,20);
+  if((position.x > width)|| (position.x < 0)){
+    velocity.x = x * - 1;
+  }
+  if ((position.y))
+}
